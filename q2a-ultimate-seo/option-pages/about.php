3e6d664c9b90e1804abe7565d45327638c76a3d6
Towhidn <Towhidn@gmail.com>
2014-10-19 00:17:32 +0330

    About Page is updated

--- q2a-ultimate-seo/option-pages/about.php
+++ q2a-ultimate-seo/option-pages/about.php
@@ -1,1 +1,25 @@
-This plugin is developed by <a href="http://towhidn.com">Towhid Nategheian</a> from <a href="http://qa-themes.com">QA-Themes.com</a>
+<p style="width: 100%; display: inline-block; text-align: center;">
+	<a href="http://qa-themes.com/">
+		<img src="http://qa-themes.com/files/q2a-theme-logo.png" alt="Question2Answer Themes" style="text-align: center;">
+	</a>
+</p>
+<p style="font-size: 115%;">
+<strong><a href="http://qa-themes.com/plugins/q2a-ultimate-seo">Question2Answer Ultimate SEO</a></strong> is developed and maintained by <a href="http://qa-themes.com">QA-Themes.com</a>.
+for more Q2A freebies and tutorials you can checkout our <a href="http://qa-themes.com/themes">Q2A Themes</a> and <a href="http://qa-themes.com/plugins">Plugins</a>.
+</p>
+<hr>
+<p style="font-size: 115%;">Hi There!</p>
+<p style="font-size: 115%;">
+This is  <a href="http://towhidn.com">Towhid</a>. the developer behind Q2A Ultimate SEO. please feel free to say hello to me on <a href="https://twitter.com/towhidn">Twitter</a>, <a href="https://www.facebook.com/pages/Towhid-Nategheian/326504900812131">FaceBook</a>, and <a href="https://plus.google.com/+Towhidn">Google+</a>.
+</p>
+<p style="font-size: 115%;">
+Also if you need professional Q2A development, I might just be the guy you were looking for.
+</p>
+<hr>
+<h3>Need Help?</h3>
+<ul>
+<li>Read my <a href="http://qa-themes.com/blog/868-seo-checklist-for-question2answer.html">Q2A SEO Checklist</a>.</li>
+<li>checkout <a href="http://qa-themes.com/forums/topic/improving-your-q2a-sites-seo">Q2A SEO Tutorial</a> on our forum.</li>
+<li>Submit issues to <a href="https://github.com/Towhidn/Q2A-Ultimate-SEO/issues/">Github</a> or send it to support forum.</li>
+</ul>
+Also complete guide to Q2A Ultimate SEO plugin is soon to come.
